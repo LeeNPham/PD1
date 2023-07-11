00b3d1df3c4c46aa629e66cfd82cbca36b1f0014
Lee Pham <leenpham@gmail.com>
2023-07-10 20:21:11 -0700

    updated slugs page

--- frontend/src/routes/pokemon/[slug]/+page.svelte
+++ frontend/src/routes/pokemon/[slug]/+page.svelte
@@ -7,5 +7,12 @@
 	});
 </script>
 
-<h1>PokeName</h1>
-<h1 class="text-4xl text-center my-8 uppercase">{data.pokeman.name}</h1>
+<div class="flex flex-col items-center">
+	<h1 class="text-4xl text-center my-8 uppercase">{data.pokeman.name}</h1>
+	<p>
+		Type: <strong>{data.pokeman.types[0].type.name}</strong> | Height:
+		<strong>{data.pokeman.height}</strong>
+		| Weight: <strong>{data.pokeman.height}</strong>
+	</p>
+	<img class="h-40 w-40" src={data.pokeman.sprites['front_default']} alt={data.pokeman.name} />
+</div>
