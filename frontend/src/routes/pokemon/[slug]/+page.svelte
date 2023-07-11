1f62bd07f714c9f613beb9a7bb7cc0724cac3b42
Lee Pham <leenpham@gmail.com>
2023-07-10 20:14:24 -0700

    added slugs

---
+++ frontend/src/routes/pokemon/[slug]/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+	import { onMount } from 'svelte';
+
+	export let data;
+	onMount(async () => {
+		console.log(data);
+	});
+</script>
+
+<h1>PokeName</h1>
+<h1 class="text-4xl text-center my-8 uppercase">{data.pokeman.name}</h1>
