32e1c4f8eb45f789653d7840e768111bae5b8f68
Lee Pham <leenpham@gmail.com>
2023-07-10 20:50:27 -0700

    added a wobble

--- frontend/src/lib/components/PokemanCard.svelte
+++ frontend/src/lib/components/PokemanCard.svelte
@@ -6,6 +6,39 @@
 	class="p-6 bg-gray-100 text-gray-800 text-center rounded-md shadow-sm hover:shadow-md flex flex-col items-center"
 	href={`/pokemon/${pokeman.id}`}
 >
-	<img class="h-40 w-40" src={pokeman.image} alt={pokeman.name} />
+	<img class="boop h-40 w-40" src={pokeman.image} alt={pokeman.name} />
 	<h2 class="uppercase text-2xl">{pokeman.id} {pokeman.name}</h2>
 </a>
+
+<style>
+	@keyframes boop {
+		0% {
+			transform: rotate(0deg);
+		}
+		20% {
+			transform: rotate(-15deg);
+		}
+		40% {
+			transform: rotate(15deg);
+		}
+		60% {
+			transform: rotate(-7deg);
+		}
+		80% {
+			transform: rotate(7deg);
+		}
+		90% {
+			transform: rotate(-3deg);
+		}
+		95% {
+			transform: rotate(2deg);
+		}
+		100% {
+			transform: rotate(0deg);
+		}
+	}
+	.boop:hover {
+		display: inline-block;
+		animation: 1s boop forwards running;
+	}
+</style>
