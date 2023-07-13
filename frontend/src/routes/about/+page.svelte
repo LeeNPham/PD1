608ea2e31e44e41ee2ef3f06eb0152c64ba6749c
Lee Pham <leenpham@gmail.com>
2023-07-13 04:50:33 -0700

    added more to about page

--- frontend/src/routes/about/+page.svelte
+++ frontend/src/routes/about/+page.svelte
@@ -1,6 +1,10 @@
 <svelte:head>
-	<title>About the Svelte Kit Pokedex</title>
+	<title>About the Author</title>
 </svelte:head>
 
 <h1 class="text-4xl text-center my-8 uppercase">About</h1>
-<p>This is a really cool sveltekit demo that uses the Pokemon API to build a Pokedex</p>
+<p>
+	This is my practice project to get used to svelte a lil more, I grabbed an image from dribble that
+	I liked so that I could simulate an actual work environment where I developed the front end and
+	its endpoints based on a UI spec.
+</p>
