2bc967b3c3acd3540fe4b6500d9dfaf3ff749a39
Lee Pham <leenpham@gmail.com>
2023-07-13 05:49:13 -0700

    updated bg

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,8 +1,14 @@
 <script>
 	import '../app.postcss';
 	import Nav from '$lib/components/Nav.svelte';
+	import PokeBall from '$lib/icons/PokeBall.svelte';
 </script>
 
+<div class="absolute">
+	<PokeBall
+		Class="h-[600px] w-[600px] fill-primary-gray/5 -translate-x-[150px] -translate-y-[150px]"
+	/>
+</div>
 <div class="w-full h-full bg-primary-background">
 	<Nav />
 	<div class="py-5 max-w-6xl mx-auto flex flex-row items-start gap-10">
