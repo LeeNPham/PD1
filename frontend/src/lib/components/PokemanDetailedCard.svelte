18e7adc4a2964dcb09cf99428137d75c98a7c428
Lee Pham <leenpham@gmail.com>
2023-07-14 04:22:42 -0700

    testing mod

--- frontend/src/lib/components/PokemanDetailedCard.svelte
+++ frontend/src/lib/components/PokemanDetailedCard.svelte
@@ -1,5 +1,12 @@
 <script context='module'>
+// export async function load({ fetch, params }) {
+// 	const id = params.slug;
+// 	const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+// 	const res = await fetch(url);
+// 	const pokeman = await res.json();
 
+// 	return { pokeman };
+// }
 </script>
 
 <script>
