e2b7d02862c1834906a9a1de48c103ea13f970ed
Lee Pham <leenpham@gmail.com>
2023-07-16 01:21:27 -0700

    added reference iframes

--- frontend/src/routes/about/+page.svelte
+++ frontend/src/routes/about/+page.svelte
@@ -13,5 +13,16 @@
 		that I liked so that I could simulate an actual work environment where I developed the front end
 		and its endpoints based on a UI spec.
 	</p>
+
+	<h1 class="view-check slide-in from-down in-view">
+		Bringing the world together <span class="nowrap">through Pokémon</span>
+	</h1>
+	<h2 class="view-check slide-in from-down in-view">Our Mission</h2>
+	<p class="view-check slide-in from-down in-view">
+		The world of Pokémon connects people across the globe, beloved by kids, adults, and every
+		Trainer in between! These incredible creatures have crossed borders and language barriers to
+		reach the hearts of millions for over 25 years, bringing people together through the joy of play
+		and discovery.
+	</p>
 	<img class="border" src={uiSpec} alt="" />
 </div>
